c9e557efb67cb8c730fdb9345cd16d74874e6017
Thomas Portelange <thomas.portelange@boomerang.be>
2014-12-01 15:17:25 +0100

    fix error placement for optionsetfield

--- javascript/zenvalidator.js
+++ javascript/zenvalidator.js
@@ -17,7 +17,10 @@
 					});
 					
         			$(this).parsley({
-            				excluded: 'input[type=button], input[type=submit], input[type=reset], input[type=hidden], :hidden, .ignore-validation'
+            				excluded: 'input[type=button], input[type=submit], input[type=reset], input[type=hidden], :hidden, .ignore-validation',
+							errorsContainer: function (el) {
+								return el.$element.closest(".field");
+							}
         			});
 			}
 		});
