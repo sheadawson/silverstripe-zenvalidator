e8e572454daad33d6ba3d6c96164dc9607b11949
Thomas <thomas@lekoala.be>
2015-07-17 13:26:51 +0200

    no conflict compat

--- code/FormActionNoValidation.php
+++ code/FormActionNoValidation.php
@@ -14,7 +14,7 @@ class FormActionNoValidation extends FormAction
 
     public function Field($properties = array())
     {
-        Requirements::customScript("$('#".$this->ID()."').click(function() { $(this).parents('form').parsley().destroy();})");
+        Requirements::customScript("jQuery('#".$this->ID()."').click(function() { $(this).parents('form').parsley().destroy();})");
         return parent::Field($properties);
     }
 }
