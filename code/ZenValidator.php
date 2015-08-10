5734d8da5888f62586bf3fde54ad0162f122258a
Shea <shea@livesource.co.nz>
2015-08-10 18:20:07 +1200

    only require entwine and zenvalidator.js if not using custom js

--- code/ZenValidator.php
+++ code/ZenValidator.php
@@ -80,9 +80,7 @@ class ZenValidator extends Validator{
 	public function applyParsley(){
 		$this->parsleyEnabled = true;
 		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
-		Requirements::javascript(THIRDPARTY_DIR.'/jquery-entwine/dist/jquery.entwine-dist.js');
 		Requirements::javascript(ZENVALIDATOR_PATH . '/javascript/parsley/parsley.remote.min.js');
-		Requirements::javascript(ZENVALIDATOR_PATH.'/javascript/zenvalidator.js');
 		
 		$lang = i18n::get_lang_from_locale(i18n::get_locale());
 		if($lang != 'en') {
@@ -92,6 +90,8 @@ class ZenValidator extends Validator{
 		if($this->form){
 			if ($this->defaultJS) {
 				$this->form->addExtraClass('parsley');
+				Requirements::javascript(THIRDPARTY_DIR.'/jquery-entwine/dist/jquery.entwine-dist.js');
+				Requirements::javascript(ZENVALIDATOR_PATH.'/javascript/zenvalidator.js');
 			}else{
 				$this->form->addExtraClass('custom-parsley');
 			}
