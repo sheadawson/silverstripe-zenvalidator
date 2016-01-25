39e8a5a153408e2fef705205b6944276fc88783e
Peter Thaleikis <petert@catalyst.net.nz>
2016-01-26 02:04:24 +1300

    style: removing not requires lines and convert indentation consistent to spaces

--- javascript/zenvalidator.js
+++ javascript/zenvalidator.js
@@ -1,262 +1,252 @@
 (function($) {
-	$.entwine('ss.zenvalidator', function($) {
+    $.entwine('ss.zenvalidator', function($) {
+        $('form.parsley').entwine({
+            onmatch: function() {
+                    // Fix validation for optionset based class (attributes not set on child options)
+                    $(this).find('.optionset').each(function() {
+                        var attrs = $.makeArray(this.attributes);
+                        var parsley = {};
+                        for(var i=0; i < attrs.length; i++) {
+                            var att = attrs[i];
+                            if(att.name.indexOf('parsley-') !== -1) {
+                                parsley[att.name] = att.value;
+                            }
+                        }
+                        $(this).find('input').attr(parsley);
 
-		$('form.parsley').entwine({
-			onmatch: function() {
-					// Fix validation for optionset based class (attributes not set on child options)
-					$(this).find('.optionset').each(function() {
-						var attrs = $.makeArray(this.attributes);
-						var parsley = {};
-						for(var i=0; i < attrs.length; i++) {
-							var att = attrs[i];
-							if(att.name.indexOf('parsley-') !== -1) {
-								parsley[att.name] = att.value;
-							}
-						}
-						$(this).find('input').attr(parsley);
+                        // Clean attributes
+                        var attributes = this.attributes;
+                        var keys = $.map(attributes , function(key, value) {
+                            return key.name;
+                        });
+                        for(var i = 0; i < keys.length; i++) {
+                            if (keys[i].indexOf('data-parsley') === 0) {
+                                $(this).removeAttr(keys[i]);
+                            }
+                        }
+                        $(this).removeData();
+                    });
 
-						// Clean attributes
-						var attributes = this.attributes;
-						var keys = $.map(attributes , function(key, value) {
-							return key.name;
-						});
-						for(var i = 0; i < keys.length; i++) {
-							if (keys[i].indexOf('data-parsley') === 0) {
-								$(this).removeAttr(keys[i]);
-							}
-						}
-						$(this).removeData();
-					});
+                    $(this).parsley({
+                            excluded: 'input[type=button], input[type=submit], input[type=reset], input[type=hidden], .ignore-validation',
+                            errorsContainer: function (el) {
+                                return el.$element.closest(".field");
+                            }
+                    });
+            }
+        });
 
-        			$(this).parsley({
-            				excluded: 'input[type=button], input[type=submit], input[type=reset], input[type=hidden], .ignore-validation',
-							errorsContainer: function (el) {
-								return el.$element.closest(".field");
-							}
-        			});
-			}
-		});
+        // Listen for error message when doing remote validation
+        // See issue https://github.com/guillaumepotier/Parsley.js/issues/560
+        $.listen('parsley:field:error', function(fieldInstance) {
+            if(!fieldInstance._xhr) {
+                return;
+            }
+            if(fieldInstance._xhr.status < 400 || fieldInstance._xhr.status > 499) {
+                return;
+            }
+            fieldInstance.options['remoteMessage'] = fieldInstance._xhr.responseText;
+        });
 
-		// Listen for error message when doing remote validation
-		// See issue https://github.com/guillaumepotier/Parsley.js/issues/560
-		$.listen('parsley:field:error', function(fieldInstance) {
-			if(!fieldInstance._xhr) {
-				return;
-			}
-			if(fieldInstance._xhr.status < 400 || fieldInstance._xhr.status > 499) {
-				return;
-			}
-			fieldInstance.options['remoteMessage'] = fieldInstance._xhr.responseText;
-		});
+        // Add action to submitted form for parsley.remote
+        // See issue https://github.com/guillaumepotier/Parsley.js/issues/826
+        var submitActor = null;
+        $('form.parsley [type=submit]').click(function() {
+            submitActor = $(this);
+        });
+        $.listen('parsley:form:success', function(formInstance) {
+            if(submitActor) {
+                formInstance.$element.find('input.parsley-submit-actor').remove();
+                formInstance.$element.append('<input class="parsley-submit-actor" type="hidden" name="'+submitActor.attr('name')+'" value="'+submitActor.attr('value')+'" />');
+            }
+            // If no action is specified, default action (the first) is used by Silverstripe (eg: if form is submitted with enter)
+        });
 
-		// Add action to submitted form for parsley.remote
-		// See issue https://github.com/guillaumepotier/Parsley.js/issues/826
-		var submitActor = null;
-		$('form.parsley [type=submit]').click(function() {
-			submitActor = $(this);
-		});
-		$.listen('parsley:form:success', function(formInstance) {
-			if(submitActor) {
-				formInstance.$element.find('input.parsley-submit-actor').remove();
-				formInstance.$element.append('<input class="parsley-submit-actor" type="hidden" name="'+submitActor.attr('name')+'" value="'+submitActor.attr('value')+'" />');
-			}
-			// If no action is specified, default action (the first) is used by Silverstripe (eg: if form is submitted with enter)
-		});
+        // Bypass validation on :hidden fields
+        $.listen('parsley:field:validate', function(fieldInstance){
+            if (fieldInstance.$element.parents('.field').is(":hidden")) {
+                fieldInstance._asyncIsValidField = function() {
+                    var deferred = $.Deferred();
+                    return deferred.resolveWith(this);
+                };
+            }
+        });
+        $.listen('parsley:field:validated', function(fieldInstance){
+            if (fieldInstance.$element.parents('.field').is(":hidden")) {
+                fieldInstance._ui.$errorsWrapper.css('display', 'none');
+                fieldInstance.validationResult = true;
+            }
+        });
 
-		// Bypass validation on :hidden fields
-		$.listen('parsley:field:validate', function(fieldInstance){
-		    if (fieldInstance.$element.parents('.field').is(":hidden")) {
-				fieldInstance._asyncIsValidField = function() {
-					var deferred = $.Deferred();
-					return deferred.resolveWith(this);
-				};
-		    }
-		});
-		$.listen('parsley:field:validated', function(fieldInstance){
-		    if (fieldInstance.$element.parents('.field').is(":hidden")) {
-		        fieldInstance._ui.$errorsWrapper.css('display', 'none');
-		        fieldInstance.validationResult = true;
-		    }
-		});
+        $('.field').entwine({
+            getFormField: function() {
+                return this.find('[name='+this.getFieldName()+'], [name="'+this.getFieldName()+'[]"]');
+            },
 
-		$('.field').entwine({
+            getFieldName: function() {
+                return this.attr('id');
+            },
 
-			getFormField: function() {
-				return this.find('[name='+this.getFieldName()+'], [name="'+this.getFieldName()+'[]"]');
-			},
+            getFieldValue: function() {
+                return this.getFormField().val();
+            },
 
-			getFieldName: function() {
-				return this.attr('id');
-			},
+            evaluateEqualTo: function(val) {
+                return this.getFieldValue() === val;
+            },
 
-			getFieldValue: function() {
-				return this.getFormField().val();
-			},
+            evaluateNotEqualTo: function(val) {
+                return this.getFieldValue() !== val;
+            },
 
-			evaluateEqualTo: function(val) {
-				return this.getFieldValue() === val;
-			},
+            evaluateLessThan: function(val) {
+                num = parseFloat(val);
+                return this.getFieldValue() < num;
+            },
 
-			evaluateNotEqualTo: function(val) {
-				return this.getFieldValue() !== val;
-			},
+            evaluateGreaterThan: function(val) {
+                num = parseFloat(val);
+                return parseFloat(this.getFieldValue()) > num;
+            },
 
-			evaluateLessThan: function(val) {
-				num = parseFloat(val);
-				return this.getFieldValue() < num;
-			},
+            evaluateContains: function(val) {
+                return this.getFieldValue().match(val) !== null;
+            },
 
-			evaluateGreaterThan: function(val) {
-				num = parseFloat(val);
-				return parseFloat(this.getFieldValue()) > num;
-			},
+            evaluateEmpty: function() {
+                return $.trim(this.getFieldValue()).length === 0;
+            },
 
-			evaluateContains: function(val) {
-				return this.getFieldValue().match(val) !== null;
-			},
+            evaluateNotEmpty: function() {
+                return !this.evaluateEmpty();
+            },
 
-			evaluateEmpty: function() {
-				return $.trim(this.getFieldValue()).length === 0;
-			},
+            evaluateChecked: function() {
+                return this.getFormField().is(":checked");
+            }
+        });
 
-			evaluateNotEmpty: function() {
-				return !this.evaluateEmpty();
-			},
+        $('.field.validation-logic').entwine({
+            onmatch: function () {
+                masters = this.getMasters();
+                for(m in masters) {
+                    this.closest('form').find('#'+masters[m]).addClass("validation-logic-master");
+                }
+            },
 
-			evaluateChecked: function() {
-				return this.getFormField().is(":checked");
-			}
+            getLogic: function() {
+                return $.trim(this.getFormField().data('validation-logic-eval'));
+            },
 
+            parseLogic: function() {
+                js = this.getLogic();
+                result = eval(js);
+                return result;
+            },
 
-		});
+            getMasters: function() {
+                var field = this.getFormField();
+                if(!field.length) {
+                    return new Array();
+                }
+                return field.data('validation-logic-masters').split(",");
+            }
+        });
 
-		$('.field.validation-logic').entwine({
-			onmatch: function () {
-				masters = this.getMasters();
-				for(m in masters) {
-					this.closest('form').find('#'+masters[m]).addClass("validation-logic-master");
-				}
-			},
+        $('.field.optionset').entwine({
+            getFormField: function() {
+                f = this._super().filter(":checked");
+                return f;
+            }
+        });
 
-			getLogic: function() {
-				return $.trim(this.getFormField().data('validation-logic-eval'));
-			},
+        $('.field.optionset.checkboxset').entwine({
+            evaluateHasCheckedOption: function(val) {
+                this.find(':checkbox').filter(':checked').each(function() {
+                    return $(this).val() === val || $(this).getLabel() === val;
+                });
+            },
 
-			parseLogic: function() {
-				js = this.getLogic();
-				result = eval(js);
-				return result;
-			},
+            evaluateHasCheckedAtLeast: function(num) {
+                return this.find(':checked').length >= num;
+            },
 
-			getMasters: function() {
-				var field = this.getFormField();
-				if(!field.length) {
-					return new Array();
-				}
-				return field.data('validation-logic-masters').split(",");
-			}
+            evaluateHasCheckedLessThan: function(num) {
+                return this.find(':checked').length <= num;
+            }
+        });
 
-		});
+        $('.field input[type=checkbox]').entwine({
+            getLabel: function() {
+                return this.closest('form').find('label[for='+this.attr('id')+']');
+            }
+        });
 
+        $('.field.validation-logic.validation-logic-validate').entwine({
+            testLogic: function() {
+                this.getFormField().toggleClass('ignore-validation', this.parseLogic());
+            }
+        });
 
-		$('.field.optionset').entwine({
+        $('.field.validation-logic.validation-logic-exclude').entwine({
+            testLogic: function() {
+                if(!this.parseLogic()){
+                    this.find('ul.parsley-errors-list').hide();
+                }else{
+                    this.find('ul.parsley-errors-list').show();
+                }
+                this.getFormField().toggleClass('ignore-validation', !this.parseLogic());
+            }
+        });
 
-			getFormField: function() {
-				f = this._super().filter(":checked");
-				return f;
-			}
+        $('.field.validation-logic-master :text, .field.validation-logic-master select').entwine({
+            onmatch: function() {
+                this.closest(".field").notify();
+            },
 
-		});
+            onchange: function() {
+                this.closest(".field").notify();
+            }
+        });
 
-		$('.field.optionset.checkboxset').entwine({
+        $('.field.validation-logic-master :checkbox, .field.validation-logic-master :radio').entwine({
+            onmatch: function() {
+                this.closest(".field").notify();
+            },
 
-			evaluateHasCheckedOption: function(val) {
-				this.find(':checkbox').filter(':checked').each(function() {
-					return $(this).val() === val || $(this).getLabel() === val;
-				});
-			},
+            onclick: function() {
+                this.closest(".field").notify();
+            }
+        });
 
-			evaluateHasCheckedAtLeast: function(num) {
-				return this.find(':checked').length >= num;
-			},
+        $('.field.validation-logic-master').entwine({
+            Listeners: null,
 
-			evaluateHasCheckedLessThan: function(num) {
-				return this.find(':checked').length <= num;
-			}
-		});
+            notify: function() {
+                $.each(this.getListeners(), function() {
+                    $(this).testLogic();
+                });
+            },
 
-		$('.field input[type=checkbox]').entwine({
-			getLabel: function() {
-				return this.closest('form').find('label[for='+this.attr('id')+']');
-			}
-		});
-
-		$('.field.validation-logic.validation-logic-validate').entwine({
-			testLogic: function() {
-				this.getFormField().toggleClass('ignore-validation', this.parseLogic());
-			}
-		});
-
-
-		$('.field.validation-logic.validation-logic-exclude').entwine({
-			testLogic: function() {
-				if(!this.parseLogic()){
-					this.find('ul.parsley-errors-list').hide();
-				}else{
-					this.find('ul.parsley-errors-list').show();
-				}
-				this.getFormField().toggleClass('ignore-validation', !this.parseLogic());
-			}
-		});
-
-		$('.field.validation-logic-master :text, .field.validation-logic-master select').entwine({
-			onmatch: function() {
-				this.closest(".field").notify();
-			},
-
-			onchange: function() {
-				this.closest(".field").notify();
-			}
-		});
-
-		$('.field.validation-logic-master :checkbox, .field.validation-logic-master :radio').entwine({
-			onmatch: function() {
-				this.closest(".field").notify();
-			},
-
-			onclick: function() {
-				this.closest(".field").notify();
-			}
-		});
-
-		$('.field.validation-logic-master').entwine({
-			Listeners: null,
-
-			notify: function() {
-				$.each(this.getListeners(), function() {
-					$(this).testLogic();
-				});
-			},
-
-			getListeners: function() {
-				if(l = this._super()) {
-					return l;
-				}
-				var self = this;
-				var listeners = [];
-				this.closest("form").find('.validation-logic').each(function() {
-					masters = $(this).getMasters();
-					for(m in masters) {
-						if(masters[m] == self.attr('id')) {
-							listeners.push($(this));
-							break;
-						}
-					}
-				});
-				this.setListeners(listeners);
-				return this.getListeners();
-			}
-		});
-	});
+            getListeners: function() {
+                if(l = this._super()) {
+                    return l;
+                }
+                var self = this;
+                var listeners = [];
+                this.closest("form").find('.validation-logic').each(function() {
+                    masters = $(this).getMasters();
+                    for(m in masters) {
+                        if(masters[m] == self.attr('id')) {
+                            listeners.push($(this));
+                            break;
+                        }
+                    }
+                });
+                this.setListeners(listeners);
+                return this.getListeners();
+            }
+        });
+    });
 
 })(jQuery);
